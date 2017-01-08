ec496bf03c115b1e3262edc8ecffba11df39f301
yh36 <yihou1003437319@hotmail.com>
2017-01-08 00:17:23 -0700

    Add main code.

--- Ninety9BottlesOfBeer/src/self/BottlesOfBeer.java
+++ Ninety9BottlesOfBeer/src/self/BottlesOfBeer.java
@@ -2,6 +2,23 @@ package self;
 
 public class BottlesOfBeer {
 	public static void main(String[] args) {
-		System.out.print("Hello Yi!");
+		int bottles = 99;
+		String word = "bottles";
+		while (bottles > 0) {
+			System.out.print(bottles + " " + word + " of beer on the wall, ");
+			System.out.println(bottles + " " + word + " of beer.");
+			bottles--;
+			if (bottles == 1) {
+				word = "bottle";
+			}
+			System.out.print("Take one down and pass it around, ");
+			if (bottles > 0) {
+				System.out.println(bottles + " " + word + " of beer on the wall.");
+			}
+			else {
+				System.out.println("No more bottles of beer on the wall, no more bottles of beer."); 
+				System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
+			}
+		}
 	}
 }
